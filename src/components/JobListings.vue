35a772ae78193f66707a7220126eaf8fe4b810ab
Sierra555 <dara6234@gmail.com>
2025-02-05 17:21:49 +0200

    cl

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -19,6 +19,7 @@ const state = reactive({
 });
 
 onMounted(async() => {
+  console.log(import.meta.env.VITE_PUBLIC_URL);
   try {
     const res = await axios.get(`${import.meta.env.VITE_PUBLIC_URL}/api/jobs`);
     state.jobs = res.data;
