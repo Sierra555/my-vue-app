c91c5ce3afaaa7c9bc41010c597bdfcea812379e
Sierra555 <dara6234@gmail.com>
2025-02-07 12:50:34 +0200

    added spinner

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <script setup>
-  import Navbar from '@/components/Navbar.vue';
-  import { RouterView } from 'vue-router';
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+import { defineAsyncComponent } from "vue";
+import { RouterView } from "vue-router";
+
+const AsyncNavbar = defineAsyncComponent(() =>
+	import("@/components/Navbar.vue")
+);
 </script>
 
 <template>
-  <Navbar />
-  <RouterView />
+	<Suspense>
+		<template #fallback>
+			<div
+				class="flex items-center justify-center h-screen"
+			>
+				<PulseLoader />
+			</div>
+		</template>
+		<template #default>
+			<div>
+				<AsyncNavbar />
+				<RouterView />
+			</div>
+		</template>
+	</Suspense>
 </template>
